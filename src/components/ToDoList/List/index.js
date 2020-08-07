920a40c5069f574a3ce8e907b4c8f233798633b0
Melissa <melissakheying@gmail.com>
2020-08-07 14:18:00 -0500

    log id of the checked list item

--- src/components/ToDoList/List/index.js
+++ src/components/ToDoList/List/index.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
-export const List = ({ todos }) => {
+export const List = ({ todos, handler }) => {
+
 
 
 
 
   return (
+    <Fragment>
     <ol>
       {todos.map(({ id, text }) => (
-        <li key={id}>{text}</li>
+        <li key={id} data-id={id}>
+        {text} <input type="checkbox" onClick={handler}/></li>
       ))}
     </ol>
+    </Fragment>
   )
 }
 
 List.propTypes = {
   todos: PropTypes.array.isRequired,
+  handler: PropTypes.func
 }
--- src/components/ToDoList/index.js
+++ src/components/ToDoList/index.js
@@ -15,12 +15,19 @@ useEffect(() => {
 }, [])
 
 
+// parentNode is the List item, child is the checkbox
+const handleCheckbox = ({target}) => {
+console.log(target.checked, target.parentNode.dataset.id)
+// TODO: If checked, find the element in todos and change 'checked' to true
+
+
+}
 
   return (
     <main>
     <h1>ToDo List</h1>
-    <List todos={todos} />
-    {/* <Input /> */}
+    <List todos={todos} handler={handleCheckbox}/>
+    <Input />
     </main>
   )
 }
